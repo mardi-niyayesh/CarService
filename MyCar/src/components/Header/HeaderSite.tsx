a115e328fa64b9d066260123c99f1720a3fcad1b
niyayesh <niyayeshammardii@gmail.com>
2025-12-07 18:13:41 +0330

    connect copmonents section Header

---
+++ MyCar/src/components/Header/HeaderSite.tsx
@@ -0,0 +1,14 @@
+import HedearBar from "./HedearBar";
+import HeroBaner from "./HeroBaner";
+import BanerHeader from "../BanerHeader";
+const HeaderSite = () => {
+  return (
+    <div>
+      <HedearBar />
+      <BanerHeader />
+      <HeroBaner />
+    </div>
+  );
+};
+
+export default HeaderSite;
