343258b2f638d50944a2fa1f126297444f7a9393
homow <homowsh897@gmail.com>
2026-02-07 04:29:59 +0330

    feat(script) for generate and migration prisma

---
+++ server/scripts/prisma-sync.ts
@@ -0,0 +1,29 @@
+import {execSync} from 'node:child_process';
+import * as readline from 'node:readline/promises';
+
+(async () => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    let migrationName: string = process.argv[2];
+    if (!migrationName) {
+        const answer: string = await rl.question('enter migration name (default = init): ');
+        migrationName = answer.trim() || 'init';
+    }
+
+    console.log(`Running migration: ${migrationName}...`);
+
+    try {
+        execSync(`npx prisma migrate dev --name ${migrationName}`, {stdio: 'inherit'});
+        execSync(`npx prisma generate`, {stdio: 'inherit'});
+
+        console.log('Migration and Prisma Client generation done!');
+    } catch (e) {
+        console.log(`Error: ${e}`);
+        process.exit(1);
+    } finally {
+        rl.close();
+    }
+})().catch(r => console.error(r));
