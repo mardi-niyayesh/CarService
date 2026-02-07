df210a57feb91bd3474180e09ca7658dfc08d970
homow <homowsh897@gmail.com>
2026-02-07 05:49:59 +0330

    remove(utils) validate UUID

--- server/src/lib/utils/db.ts
+++ server/src/lib/utils/db.ts
@@ -1,10 +1,4 @@
 import z from 'zod';
-import {IsUUID} from "class-validator";
-
-export class IsValidUUIDv4 {
-  @IsUUID("4", {message: "UUID v4 is invalid, please enter a valid UUID"})
-  id: string;
-}
 
 export function formatZodError(error: z.ZodError) {
   return error?.issues?.map(i => ({
