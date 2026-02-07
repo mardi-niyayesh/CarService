973b96c35e8d01a5660e2663f62e45702f0b6ad2
homow <homowsh897@gmail.com>
2026-02-07 06:03:20 +0330

    refactor(directories) move pipes to library

--- server/src/lib/index.ts
+++ server/src/lib/index.ts
@@ -2,3 +2,4 @@ export * from "./dto";
 export * from "./types";
 export * from "./utils";
 export * from "./interceptors";
+export * from "./pipes";
--- server/src/lib/pipes/zod.pipe.ts
+++ server/src/lib/pipes/zod.pipe.ts
@@ -1,5 +1,5 @@
 import z from "zod";
-import {formatZodError} from "../lib";
+import {formatZodError} from "../index";
 import {Injectable, PipeTransform, BadRequestException} from '@nestjs/common';
 
 @Injectable()
