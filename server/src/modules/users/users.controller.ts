2e7abedc935c61fbaae74fc111f0b553c6564213
homow <homowsh897@gmail.com>
2026-02-08 18:45:46 +0330

    clean codes

--- server/src/modules/users/users.controller.ts
+++ server/src/modules/users/users.controller.ts
@@ -7,18 +7,14 @@ import {
   ApiCookieAuth,
 } from "@nestjs/swagger";
 import * as UserDTO from "./dto";
-import type {Request} from "express";
 import {UsersService} from "./users.service";
-import {AccessGuard, RefreshGuard} from "../auth/guards/guards";
-import {Controller, Get, Param, Req, UseGuards} from '@nestjs/common';
+import {Controller, Get, Param} from '@nestjs/common';
 import {BadRequestUUIDParams, UUID4Dto, UUID4Schema, type UUID4Type, ZodPipe} from "../../common";
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @UseGuards(AccessGuard)
-  @UseGuards(RefreshGuard)
   @Get(":id")
   @ApiParam(UUID4Dto)
   @ApiOkResponse({type: UserDTO.GetUserOkResponse})
@@ -27,11 +23,8 @@ export class UsersController {
   @ApiBearerAuth("accessToken")
   @ApiCookieAuth("refreshToken")
   findOne(
-    @Req() req: Request,
     @Param(new ZodPipe(UUID4Schema)) params: UUID4Type,
   ) {
-    console.log(req.headers);
-    console.log(req.cookies);
     return this.usersService.findOne(params.id);
   }
 }
