bb157ed9860ebd010708de8020a8154c37a9ab22
niyayesh <niyayeshammardii@gmail.com>
2026-02-08 08:40:28 -0800

    add section comment to MainSite

--- client/src/components/Main/MainSite.tsx
+++ client/src/components/Main/MainSite.tsx
@@ -2,6 +2,10 @@ import CarParts from "./CarParts";
 import WhyAutoRent from "./WhyAutoRent";
 import Slider from "./Slider";
 import ReserveAutorent from "./ReserveAutorent";
+
+import FeacherBoxcommentUser from "../comment/FeacherBoxcommentUser";
+
+
 const MainSite = () => {
   return (
     <>
@@ -9,6 +13,8 @@ const MainSite = () => {
       <WhyAutoRent />
       <Slider />
       <ReserveAutorent />
+      <FeacherBoxcommentUser/>
+      
     </>
   );
 };
