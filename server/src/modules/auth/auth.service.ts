52b910bc0dbcbbb23c59f0a3eef60ac1151832d1
homow <homowsh897@gmail.com>
2026-02-08 11:27:04 +0330

    refactor(structure) move create user to auth route

--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -1,4 +1,38 @@
-import {Injectable} from '@nestjs/common';
+import {ConflictException, Injectable} from '@nestjs/common';
+import * as UserDto from "../users/dto";
+import {PrismaService} from "../prisma/prisma.service";
+import {User, UserRole} from "../prisma/generated/client";
+import {BaseApiResponseType, CreateUserResponse} from "../../lib";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  /** create user in db */
+  async register(createData: UserDto.CreateUserInput): Promise<BaseApiResponseType<CreateUserResponse>> {
+    const user: User | null = await this.prisma.user.findFirst({
+      where: {
+        email: createData.email,
+      }
+    });
+
+    if (user) throw new ConflictException('User already exists');
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        ...createData,
+        role: UserRole.USER
+      }
+    });
+
+    return {
+      message: "user created successfully",
+      data: {
+        user: {
+          ...newUser,
+          password: undefined
+        }
+      }
+    };
+  }
+}
--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -1,41 +1,10 @@
-import * as UserDTO from "./dto";
-import {User} from "../prisma/generated/client";
-import {UserRole} from "../prisma/generated/enums";
 import {PrismaService} from "../prisma/prisma.service";
+import {Injectable, NotFoundException} from '@nestjs/common';
 import {BaseApiResponseType, type CreateUserResponse} from "../../lib";
-import {ConflictException, Injectable, NotFoundException} from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
-
-  /** create user in db */
-  async create(createData: UserDTO.CreateUserInput): Promise<BaseApiResponseType<CreateUserResponse>> {
-    const user: User | null = await this.prisma.user.findFirst({
-      where: {
-        email: createData.email,
-      }
-    });
-
-    if (user) throw new ConflictException('User already exists');
-
-    const newUser = await this.prisma.user.create({
-      data: {
-        ...createData,
-        role: UserRole.USER
-      }
-    });
-
-    return {
-      message: "user created successfully",
-      data: {
-        user: {
-          ...newUser,
-          password: undefined
-        }
-      }
-    };
-  }
 
   async findOne(id: string): Promise<BaseApiResponseType<CreateUserResponse>> {
     const user = await this.prisma.user.findFirst({
