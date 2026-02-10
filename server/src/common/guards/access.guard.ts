1d0e435bd4f786a5c863be623bf717c4c7342258
homow <homowsh897@gmail.com>
2026-02-10 20:15:01 +0330

    feat(decorator) use public decorator for access guard

--- server/src/common/guards/access.guard.ts
+++ server/src/common/guards/access.guard.ts
@@ -1,8 +1,22 @@
+import {Reflector} from "@nestjs/core";
 import type {UserAccess} from "@/types";
+import {PUBLIC_METADATA} from "@/common";
 import {AuthGuard} from "@nestjs/passport";
-import {ExecutionContext, UnauthorizedException} from "@nestjs/common";
+import {CanActivate, ExecutionContext, UnauthorizedException} from "@nestjs/common";
 
-export class AccessTokenGuard extends AuthGuard("jwt-access") {
+export class AccessTokenGuard extends AuthGuard("jwt-access") implements CanActivate {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic: boolean = this.reflector.get<boolean>(PUBLIC_METADATA, context.getHandler());
+
+    if (isPublic) return true;
+
+    return super.canActivate(context);
+  }
+
   // noinspection JSUnusedGlobalSymbols
   handleRequest<T extends UserAccess>(err: Error, user: T, _info: unknown, _context: ExecutionContext, _status?: unknown) {
     if (err || !user) throw new UnauthorizedException({
