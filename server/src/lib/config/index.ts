f4c95da7cf627a2ac07798e2f8cae1af0f46b408
homow <homowsh897@gmail.com>
2026-02-11 15:44:23 +0330

    refactor(NODE_ENV) set global isProduction in common

--- server/src/lib/config/index.ts
+++ server/src/lib/config/index.ts
@@ -1,1 +1,2 @@
+export * from "./common";
 export * from "./throttler.config";
--- server/src/lib/config/throttler.config.ts
+++ server/src/lib/config/throttler.config.ts
@@ -1,13 +1,15 @@
+import {isProduction} from "./";
 import type {ThrottlerModuleOptions} from "@nestjs/throttler";
 
 const ONE_MINUTE = 60_000;
-const BLOCK_DURATION = ONE_MINUTE * 5;
+const BLOCK_DURATION: number = ONE_MINUTE * 5;
 
 export const throttlerConfig: ThrottlerModuleOptions = {
   throttlers: [{
-    ttl: process.env.NODE_ENV === 'production' ? ONE_MINUTE : 1000,
-    limit: process.env.NODE_ENV === 'production' ? 10 : 15,
-    blockDuration: process.env.NODE_ENV === 'production' ? ONE_MINUTE * 5 : ONE_MINUTE * 10,
+    ttl: isProduction ? ONE_MINUTE : 1000,
+    limit: isProduction ? 10 : 15,
+    blockDuration: isProduction ? ONE_MINUTE * 5 : ONE_MINUTE * 10,
+
   }],
-  errorMessage: () => `Too many requests. Try again ${BLOCK_DURATION / ONE_MINUTE} minutes later.`,
+  errorMessage: (): string => `Too many requests. Try again ${BLOCK_DURATION / ONE_MINUTE} minutes later.`,
 };
