ed61d871bb51f875a0c32c20398ec6a9356c25fd
homow <homowsh897@gmail.com>
2026-02-12 09:58:55 +0330

    refactor(imports)

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,13 +1,13 @@
 import request from 'supertest';
 import {App} from 'supertest/types';
-import {AppModule} from '../src/app.module';
+import {AppModule} from '@/app.module';
 import {INestApplication} from '@nestjs/common';
 import {Test, TestingModule} from '@nestjs/testing';
 
-describe('AppController (e2e)', () => {
+describe('AppController (e2e)', (): void => {
   let app: INestApplication<App>;
 
-  beforeEach(async () => {
+  beforeAll(async (): Promise<void> => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -16,10 +16,13 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/ GET ', () => {
     return request(app.getHttpServer())
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expect(200);
+  });
+
+  afterAll(async (): Promise<void> => {
+    await app.close();
   });
 });
