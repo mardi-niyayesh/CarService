156b07287064d4d79bbe2acf01ac8d77f20ed1cd
homow <homowsh897@gmail.com>
2026-02-12 12:05:48 +0330

    refactor(config) vitest

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
-    setupFiles: ['./test/setup.ts'],
     include: ['**/*.spec.ts', '**/*.test.ts'],
     exclude: ['**/node_modules/**', '**/dist/**'],
     coverage: {
