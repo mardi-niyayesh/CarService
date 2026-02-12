33924ce4a76fed19a81e47a0db6572d333b1563a
niyayesh <niyayeshammardii@gmail.com>
2026-02-12 07:49:08 -0800

    add BrowserRouter, Routes, Route to react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HeaderSite from "./components/Header/HeaderSite";
-import MainSite from "./components/Main/MainSite";
 import Footer from "./components/Footer/Footer";
+//pages
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 function App() {
   return (
     <>
-      <HeaderSite/>
-      <MainSite/>
-      <Footer/>
+      <BrowserRouter>
+        <HeaderSite />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
