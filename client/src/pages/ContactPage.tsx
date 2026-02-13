b48fdaae82476e96d9e274f543544a1911469ece
niyayesh <niyayeshammardii@gmail.com>
2026-02-13 08:26:36 -0800

    creat section contactus

--- client/src/pages/ContactPage.tsx
+++ client/src/pages/ContactPage.tsx
@@ -6,7 +6,7 @@ const ContactPage = () => {
     <>
       <div className="bg-[url('../../assets/page.png')] bg-cover bg-center bg-no-repeat w-full min-h-[250px] md:min-h-[400px] lg:min-h-[550px] xl:h-[683px] relative"></div>
 
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-[1200px]">
+      <div className="container rounded-2xl mx-auto px-4 sm:px-6 lg:px-8 max-w-[1200px]  mt-10 mb-10 bg-[#EDEDED] ">
         <div className="flex flex-col lg:flex-row items-stretch gap-8 lg:gap-12 py-8 md:py-12 lg:py-16">
           <div className="w-full lg:w-1/2 flex flex-col justify-center space-y-6 md:space-y-8">
             <div className="text-[#494949] text-xl md:text-2xl lg:text-3xl font-bold relative pb-3 inline-block">
@@ -68,7 +68,6 @@ const ContactPage = () => {
               src={location}
               alt="location map"
               className="w-full h-full object-cover rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
-            
             />
           </div>
         </div>
