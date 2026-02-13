e3b63b4903fabad8e981108b3031ffad96d6cda2
homow <homowsh897@gmail.com>
2026-02-13 14:58:29 +0330

    refactor(type) split void and genric types in response data

--- server/src/types/response.types.ts
+++ server/src/types/response.types.ts
@@ -6,4 +6,4 @@ export interface BaseApiResponseData<T> extends BaseApiResponse {
   data: T;
 }
 
-export type ApiResponse<T extends void> = T extends void ? BaseApiResponse : BaseApiResponseData<T>;
+export type ApiResponse<T> = T extends void ? BaseApiResponse : BaseApiResponseData<T>;
