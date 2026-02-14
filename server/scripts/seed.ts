e86583ad9c81aa5f4a57f33c8b80aa328a2261c1
homow <homowsh897@gmail.com>
2026-02-14 23:18:40 +0330

    feat(script) add new for seed data in database

---
+++ server/scripts/seed.ts
@@ -0,0 +1,27 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+import {CliModule} from "@/modules";
+import "tsconfig-paths/register.js";
+import {NestFactory} from "@nestjs/core";
+import {PrismaService} from "@/modules/prisma/prisma.service";
+
+await (async (): Promise<void> => {
+  const app = await NestFactory.createApplicationContext(CliModule);
+
+  const prisma = app.get(PrismaService);
+
+  await prisma.role.createMany({
+    data: [
+      {
+        name: "SELF",
+        description: "Basic role for users to view and update their own personal information"
+      },
+      {
+        name: "OWNER",
+        description: "System owner with full access to all resources"
+      }
+    ]
+  });
+})();
