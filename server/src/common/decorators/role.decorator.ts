589ccb3997eed05ce8e4ba7b77f48179462a889c
homow <homowsh897@gmail.com>
2026-02-14 09:28:44 +0330

    refactor(role-structure) role decorator

--- server/src/common/decorators/role.decorator.ts
+++ server/src/common/decorators/role.decorator.ts
@@ -1,5 +1,4 @@
 import {SetMetadata} from "@nestjs/common";
-import {UserRole} from "@/modules/prisma/generated/enums";
 
 export const ROLE_METADATA = "role";
-export const Role = (role: UserRole) => SetMetadata(ROLE_METADATA, role);
+export const Role = (...roles: string[]) => SetMetadata(ROLE_METADATA, roles);
