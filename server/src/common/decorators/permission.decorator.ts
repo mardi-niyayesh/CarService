bba38bd9ce351d150b0ebbdb56319bc284d5a654
homow <homowsh897@gmail.com>
2026-02-14 10:06:02 +0330

    refactor(permission-structure in decorator) change 'role' to 'permission'

---
+++ server/src/common/decorators/permission.decorator.ts
@@ -0,0 +1,4 @@
+import {SetMetadata} from "@nestjs/common";
+
+export const PERMISSION_METADATA = "permission";
+export const Permission = (requireAll?: boolean, ...permission: string[]) => SetMetadata(PERMISSION_METADATA, {requireAll, permission});
