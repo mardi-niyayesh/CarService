695ce82e3dbd3f653631ed07055fac8e0931eb43
homow <homowsh897@gmail.com>
2026-02-17 23:29:57 +0330

    refactor(style)

--- server/scripts/seed-roles.ts
+++ server/scripts/seed-roles.ts
@@ -7,7 +7,47 @@ import {BaseRoles} from "@/types";
 import {PERMISSIONS} from "@/common";
 import {NestFactory} from "@nestjs/core";
 import {CliModule} from "@/modules/cli/cli.module";
+import {INestApplicationContext} from "@nestjs/common";
 import {PrismaService} from "@/modules/prisma/prisma.service";
+
+/** create user manage role */
+async function createUserManagerRole(app: INestApplicationContext, prisma: PrismaService): Promise<void> {
+  const userManagerRole = await prisma.role.create({
+    data: {
+      name: BaseRoles.user_manager,
+      description: "Full administrative access to manage all users in the system"
+    }
+  });
+
+  if (!userManagerRole) {
+    console.log("⚠ Something went wrong in Server! userManagerRole not created!");
+    await app.close();
+    process.exit(1);
+  }
+
+  const userManagerPermission = await prisma.permission.findMany({
+    where: {
+      name: {
+        startsWith: PERMISSIONS.USER_VIEW.split(".")[0]
+      }
+    }
+  });
+
+  if (!userManagerPermission) {
+    console.log("⚠ Something went wrong in Server! userManagerPermission not created");
+    await app.close();
+    process.exit(1);
+  }
+
+  for (const p of userManagerPermission) {
+    await prisma.rolePermission.create({
+      data: {
+        role_id: userManagerRole.id,
+        permission_id: p.id,
+      }
+    });
+  }
+}
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.createApplicationContext(CliModule);
@@ -48,18 +88,10 @@ async function bootstrap(): Promise<void> {
     }
   });
 
-  const userManagerRole = await prisma.role.create({
-    data: {
-      name: BaseRoles.user_manager,
-      description: "Full administrative access to manage all users and in the system"
-    }
-  });
-
   if (
     !ownerRole
     || !selfPermission
     || !selfRole
-    || !userManagerRole
   ) {
     console.log("⚠ Something went wrong in Server! ownerRole or selfPermission or selfRole or userManagerRole not created");
     await app.close();
@@ -73,28 +105,7 @@ async function bootstrap(): Promise<void> {
     }
   });
 
-  const userManagerPermission = await prisma.permission.findMany({
-    where: {
-      name: {
-        startsWith: PERMISSIONS.USER_VIEW.split(".")[0]
-      }
-    }
-  });
-
-  if (!userManagerPermission) {
-    console.log("⚠ Something went wrong in Server! userManagerPermission not created");
-    await app.close();
-    process.exit(1);
-  }
-
-  for (const p of userManagerPermission) {
-    await prisma.rolePermission.create({
-      data: {
-        role_id: userManagerRole.id,
-        permission_id: p.id,
-      }
-    });
-  }
+  await createUserManagerRole(app, prisma);
 
   console.log("✅ Seed completed: Default roles and permissions have been created successfully.");
 
