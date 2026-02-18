92470aee35c2dce5b734d698882f4fb58b44c8a5
homow <homowsh897@gmail.com>
2026-02-18 05:59:41 +0330

    feat(permissions) add roles permissions

--- server/src/common/constants/permissions.ts
+++ server/src/common/constants/permissions.ts
@@ -5,8 +5,17 @@ const USER = {
   USER_DELETE: 'user.delete',
 } as const;
 
+const ROLE = {
+  ROLE_ASSIGN: "role.assign",
+  ROLE_CREATE: "role.create",
+  ROLE_VIEW: 'role.view',
+  ROLE_UPDATE: 'role.update',
+  ROLE_DELETE: 'role.delete',
+};
+
 export const PERMISSIONS = {
   ...USER,
+  ...ROLE,
 } as const;
 
 export type PermissionsType = typeof PERMISSIONS[keyof typeof PERMISSIONS];
