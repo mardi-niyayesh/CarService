b7fae0a2256315197c21f5c9866ec9081b798244
homow <homowsh897@gmail.com>
2026-02-19 08:56:44 +0330

    refactor(Schema)

--- server/src/modules/users/dto/users.validators.ts
+++ server/src/modules/users/dto/users.validators.ts
@@ -7,8 +7,8 @@ export const BaseUserSchema = z.object({
     .min(6)
     .max(20)
     .regex(
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]+$/,
-      {error: "password must contain at least one letter, one number, and one special character (!@#$%^&*)"}
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*]+$/,
+      {error: "password must contain at least one letter and one number"}
     ),
   display_name: z.string().min(3).max(40).optional(),
   age: z.number().min(0).max(120).optional(),
