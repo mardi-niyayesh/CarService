845b3ae143b1f2a736ae72c030b4bf9a809bc113
niyayesh <niyayeshammardii@gmail.com>
2026-02-19 02:23:40 -0800

    add typs RegisterForm and LoginForm and typ for props componnent

--- client/src/types/auth.types.ts
+++ client/src/types/auth.types.ts
@@ -5,3 +5,29 @@ export type FormData = {
   age: number;
   rules: boolean;
 };
+
+// type for props components
+ export type FormInputProps = {
+  label: string;
+  name: string;
+  type?: string;
+  placeholder?: string;
+  register: any;
+  error?: any;
+  validation?: object;
+};
+
+//type for Register Form
+export type RegisterFormData = {
+  email: string;
+  password: string;
+  firstname: string;
+  age: number;
+  rules: boolean;
+};
+
+//type for Login Form
+export type LoginFormData = {
+  email: string;
+  password: string;
+};
