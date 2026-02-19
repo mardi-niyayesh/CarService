5217e8cc1b4274f00e354388ed938d9dd32ec1e4
niyayesh <niyayeshammardii@gmail.com>
2026-02-19 02:19:59 -0800

    add api login form

--- client/src/services/api.tsx
+++ client/src/services/api.tsx
@@ -1,4 +1,5 @@
 import { type FormData } from "../types/auth.types";
+import { type LoginFormData } from "../types/auth.types";
 
 export const registerUser = async (userData: FormData) => {
   const response = await fetch("http://localhost:3000/api/auth/register", {
@@ -11,6 +12,22 @@ export const registerUser = async (userData: FormData) => {
 
   if (!response.ok) {
     throw new Error("خطا در ثبت‌نام");
+  }
+
+  return await response.json();
+};
+
+export const loginUser = async (userData: LoginFormData) => {
+  const response = await fetch("http://localhost:3000/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+
+  if (!response.ok) {
+    throw new Error("خطا در ورود");
   }
 
   return await response.json();
