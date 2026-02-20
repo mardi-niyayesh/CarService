53bd1936e62fe6ffd918465ef0a935cef3f371a2
niyayesh <niyayeshammardii@gmail.com>
2026-02-20 00:37:27 -0800

    refactor setting prxci

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,20 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+
+        configure: (proxy, _options) => {
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("🚀 پراکسی:", req.method, req.url, "→", proxyReq.path);
+          });
+        },
+      },
+    },
+  },
 });
