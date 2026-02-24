e5812467e84073565aff63b9111c440b4751ab79
homow <homowsh897@gmail.com>
2026-02-24 06:31:00 +0330

    refactor(service) move reset-password logic to transaction

--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -78,7 +78,7 @@ export class AuthService {
         }
       });
 
-      const permissions = [...new Set(
+      const permissions: string[] = [...new Set(
         roles.role.rolePermissions.map(p => p.permission.name)
       )];
 
@@ -223,111 +223,115 @@ export class AuthService {
 
   /** send email to user for reset password */
   async forgotPassword(to: string): Promise<ApiResponse<AuthDto.ForgotApiResponse>> {
-    const user = await this.prisma.user.findUnique({
-      where: {
-        email: to
-      },
-      include: {
-        passwordToken: true
-      }
-    });
-
-    if (!user) throw new NotFoundException({
-      message: 'User not found in database',
-      error: 'User Not Found'
-    } as BaseException);
-
-    if (user.passwordToken) throw new ConflictException({
-      message: 'A password reset token is already active. Please check your Inbox Email or spam.',
-      error: 'Email Already Send'
-    } as BaseException);
-
-    const token: string = generateRandomToken();
-    const hashedToken: string = hashSecretToken(token);
-
-    const expireMinutes: number = 15;
-
-    const templatePath: string = path.join(process.cwd(), "public/html/forgot-password.html");
-
-    let html: string;
-    try {
-      html = readFileSync(templatePath, 'utf8');
-
-      const resetLink: string = `${process.env.CLIENT_RESET_PASSWORD}?token=${token}`;
-
-      html = html.replace("{{resetLink}}", resetLink);
-      html = html.replace("{{expireMinutes}}", expireMinutes.toString());
-    } catch (err) {
-      throw new InternalServerErrorException({
-        message: 'Failed to load email template',
-        error: (err as Error).message,
-      } as BaseException);
-    }
-
-    try {
-      await this.emailService.sendHtmlEmail(to, html);
-
-      await this.prisma.passwordToken.create({
-        data: {
-          user_id: user.id,
-          token: hashedToken,
-          expires_at: new Date(Date.now() + expireMinutes * 60 * 1000)
+    return this.prisma.$transaction(async tx => {
+      const user = await tx.user.findUnique({
+        where: {
+          email: to
+        },
+        include: {
+          passwordToken: true
         }
       });
 
-      return {
-        message: "Email sent successfully, Please check your inbox",
-        data: {
-          email: to,
-          time: `${expireMinutes} minutes left`,
-          timeNumber: expireMinutes
-        }
-      };
-    } catch (e) {
-      throw new InternalServerErrorException({
-        message: (e as Error).message || 'Failed to send reset password email',
-        error: (e as Error).name || "Internal Server Error",
+      if (!user) throw new NotFoundException({
+        message: 'User not found in database',
+        error: 'User Not Found'
       } as BaseException);
-    }
+
+      if (user.passwordToken) throw new ConflictException({
+        message: 'A password reset token is already active. Please check your Inbox Email or spam.',
+        error: 'Email Already Send'
+      } as BaseException);
+
+      const token: string = generateRandomToken();
+      const hashedToken: string = hashSecretToken(token);
+
+      const expireMinutes: number = 15;
+
+      const templatePath: string = path.join(process.cwd(), "public/html/forgot-password.html");
+
+      let html: string;
+      try {
+        html = readFileSync(templatePath, 'utf8');
+
+        const resetLink: string = `${process.env.CLIENT_RESET_PASSWORD}?token=${token}`;
+
+        html = html.replace("{{resetLink}}", resetLink);
+        html = html.replace("{{expireMinutes}}", expireMinutes.toString());
+      } catch (err) {
+        throw new InternalServerErrorException({
+          message: 'Failed to load email template',
+          error: (err as Error).message,
+        } as BaseException);
+      }
+
+      try {
+        await this.emailService.sendHtmlEmail(to, html);
+
+        await tx.passwordToken.create({
+          data: {
+            user_id: user.id,
+            token: hashedToken,
+            expires_at: new Date(Date.now() + expireMinutes * 60 * 1000)
+          }
+        });
+
+        return {
+          message: "Email sent successfully, Please check your inbox",
+          data: {
+            email: to,
+            time: `${expireMinutes} minutes left`,
+            timeNumber: expireMinutes
+          }
+        };
+      } catch (e) {
+        throw new InternalServerErrorException({
+          message: (e as Error).message || 'Failed to send reset password email',
+          error: (e as Error).name || "Internal Server Error",
+        } as BaseException);
+      }
+    });
   }
 
   /** Reset password with token */
   async resetPassword(token: string, password: string): Promise<ApiResponse<void>> {
-    const hashedToken: string = hashSecretToken(token);
+    return this.prisma.$transaction(async tx => {
+      const hashedToken: string = hashSecretToken(token);
 
-    const findToken = await this.prisma.passwordToken.findFirst({
-      where: {token: hashedToken},
-      include: {user: true}
+      const findToken = await tx.passwordToken.findFirst({
+        where: {token: hashedToken},
+        include: {user: true}
+      });
+
+      if (!findToken) throw new NotFoundException({
+        message: 'Reset password token is invalid or has expired.',
+        error: "Token Not Found"
+      } as BaseException);
+
+      if (findToken.expires_at < new Date()) throw new BadRequestException({
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'The reset password token has expired. Please request a new one.',
+        error: 'Token Expired'
+      });
+
+      const newPassword: string = await hashSecret(password);
+
+      await tx.user.update({
+        where: {
+          id: findToken.user.id
+        },
+        data: {
+          password: newPassword
+        }
+      });
+
+      await tx.passwordToken.delete({
+        where: {id: findToken.id}
+      });
+
+      return {
+        message: "Password reset successfully",
+      };
     });
-
-    if (!findToken) throw new NotFoundException({
-      message: 'Reset password token is invalid or has expired.',
-      error: "Token Not Found"
-    } as BaseException);
-
-    if (findToken.expires_at < new Date()) throw new BadRequestException({
-      statusCode: HttpStatus.BAD_REQUEST,
-      message: 'The reset password token has expired. Please request a new one.',
-      error: 'Token Expired'
-    });
-
-    const newPassword: string = await hashSecret(password);
-
-    await this.prisma.user.update({
-      where: {
-        id: findToken.user.id
-      },
-      data: {
-        password: newPassword
-      }
-    });
-
-    await this.prisma.passwordToken.delete({
-      where: {id: findToken.id}
-    });
-
-    return {
-      message: "Password reset successfully",
-    };
   }
 }
