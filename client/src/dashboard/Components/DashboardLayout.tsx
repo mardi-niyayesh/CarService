4efaa6c07946fd62ade3ad139b9823b3b11d6e59
niyayesh <niyayeshammardii@gmail.com>
2026-02-25 01:29:49 -0800

    creat  component DashboardLayout

---
+++ client/src/dashboard/Components/DashboardLayout.tsx
@@ -0,0 +1,13 @@
+import DashboardSidebar from "./DashboardSidebar";
+
+const DashboardLayout = () => {
+  return (
+    <>
+    <div>
+       <DashboardSidebar/>
+    </div>
+    </>
+  )
+};
+
+export default DashboardLayout;
