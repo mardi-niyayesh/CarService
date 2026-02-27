f3d7682223e1e7859d2fe8ff1a5bf4a714fb7c97
niyayesh <niyayeshammardii@gmail.com>
2026-02-27 08:48:51 -0800

    error handeling to WarningModal

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,11 +1,17 @@
+//hooks
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+//components
 import AuthForm from "../components/common/AuthForm";
+// types
 import { type RegisterFormData } from "../types/auth.types";
+//api
 import { registerUser } from "../services/api";
+//Modals
 import SuccessModal from "../components/common/SuccessModal";
 import ErrorModal from "../components/common/ErrorModal";
-import { useState } from "react";
+import WarningModal from "../components/common/WarningModal ";
 
 function RegisterPage() {
   const navigate = useNavigate();
@@ -15,21 +21,44 @@ function RegisterPage() {
   //ErrorModal
   const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  //WarningModal
+  const [isWarningModalOpen, setIsWarningModalOpen] = useState(false);
+  const [WarningMessage, setWarningMessage] = useState("");
 
   const mutation = useMutation({
     mutationFn: registerUser,
-    onSuccess: () => {
-      setModalMessage(
-        "ثبت‌نام شما با موفقیت انجام شد! به خانواده کارسرویس خوش آمدید.",
-      );
-      setIsModalOpen(true);
- 
+    onSuccess: (data) => {
+      //error 400
+      if (data.statusCode === 400) {
+        setWarningMessage(`اطلاعات شما با قوانین ثبت‌نام همخوانی ندارد.
+      لطفاً مطمئن شوید:
+       • ایمیل صحیح است
+      • رمز عبور حداقل ۶ کاراکتر (شامل حرف و عدد)
+      • نام کاربری حداقل ۳ کاراکتر
+      • سن بین ۰ تا ۱۲۰ سال`);
+        setIsWarningModalOpen(true);
+      }
+      //error 409
+      else if (data.statusCode === 409) {
+        setWarningMessage("کاربری با این مشخصات قبلا ثبت نام کرده است");
+        setIsWarningModalOpen(true);
+      }
+      //error 500
+      else if (data.statusCode === 500) {
+        setErrorMessage("خطای سرور. لطفاً بعداً دوباره تلاش کنید.");
+        setIsErrorModalOpen(true);
+      }
+      //success
+      else {
+        setModalMessage(
+          "ثبت‌نام شما با موفقیت انجام شد! به خانواده کارسرویس خوش آمدید.",
+        );
+        setIsModalOpen(true);
+      }
     },
     onError: (error: Error) => {
       console.error("خطا:", error);
-      setErrorMessage(
-        error.message || "خطایی در ثبت‌نام رخ داد. لطفاً مجدداً تلاش کنید.",
-      );
+      setErrorMessage("خطایی در ثبت‌نام رخ داد. لطفاً مجدداً تلاش کنید.");
       setIsErrorModalOpen(true);
     },
   });
@@ -40,11 +69,15 @@ function RegisterPage() {
   //SuccessModal
   const handleCloseModal = () => {
     setIsModalOpen(false);
-    navigate("/login")
+    navigate("/login");
   };
   //SuccessModal
   const handleCloseErrorModal = () => {
     setIsErrorModalOpen(false);
+  };
+  // WarningModal
+  const handleCloseWarningModal = () => {
+    setIsWarningModalOpen(false);
   };
 
   return (
@@ -64,6 +97,12 @@ function RegisterPage() {
         isOpen={isErrorModalOpen}
         onClose={handleCloseErrorModal}
         message={errorMessage}
+      />
+      <WarningModal
+        isOpen={isWarningModalOpen}
+        onClose={handleCloseWarningModal}
+        message={WarningMessage}
+        title="توجه"
       />
     </>
   );
