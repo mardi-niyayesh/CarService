f4962c77befdda3e8244479060f2a5f7d174276d
niyayesh <niyayeshammardii@gmail.com>
2026-02-27 09:59:24 -0800

    compleat Resetpassword

--- client/src/pages/ResetPasswordPage.tsx
+++ client/src/pages/ResetPasswordPage.tsx
@@ -1,5 +1,137 @@
+//components
+import AuthForm from "../components/common/AuthForm";
+//Modal
+import SuccessModal from "../components/common/SuccessModal";
+import ErrorModal from "../components/common/ErrorModal";
+import WarningModal from "../components/common/WarningModal ";
+//hooks
+import { useState } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+//api
+import { resetPassword } from "../services/api";
+
 const ResetPasswordPage = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get("token");
+
+  // SuccessModal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+
+  // ErrorModal
+  const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // WarningModal
+  const [isWarningModalOpen, setIsWarningModalOpen] = useState(false);
+  const [warningMessage, setWarningMessage] = useState("");
+
+  const { mutate, isPending, error } = useMutation({
+    mutationFn: resetPassword,
+    onSuccess: (data) => {
+      console.log("پاسخ سرور:", data);
+
+      //success
+      if (data.success === true) {
+        setModalMessage(data.message || "رمز عبور شما با موفقیت تغییر کرد");
+        setIsModalOpen(true);
+        setTimeout(() => {
+          navigate("/dashboard");
+        }, 3000);
+      }
+      // error 400
+      else if (data.statusCode === 400) {
+        setWarningMessage(
+          "اطلاعات وارد شده معتبر نیست.\n" +
+            "لطفاً رمز عبور جدید را با شرایط زیر وارد کنید:\n" +
+            "• حداقل ۶ کاراکتر\n" +
+            "• شامل حروف و اعداد",
+        );
+        setIsWarningModalOpen(true);
+      }
+      // error 401
+      else if (data.statusCode === 401) {
+        setWarningMessage(
+          " لینک بازیابی رمز عبور منقضی شده است.\n" +
+            "لطفاً مجدداً درخواست بازیابی رمز بدهید.",
+        );
+        setIsWarningModalOpen(true);
+        setTimeout(() => {
+          navigate("/forgot-password");
+        }, 3000);
+      }
+      // error 409
+      else if (data.statusCode === 409) {
+        setWarningMessage(
+          " این لینک قبلاً استفاده شده است.\n" +
+            "اگر نیاز به تغییر رمز دارید، مجدداً درخواست بدهید.",
+        );
+        setIsWarningModalOpen(true);
+        setTimeout(() => {
+          navigate("/forgot-password");
+        }, 3000);
+      }
+      //other error
+      else {
+        setErrorMessage("خطایی رخ داد. لطفاً دوباره تلاش کنید.");
+        setIsErrorModalOpen(true);
+      }
+    },
+    onError: (error: any) => {
+      console.error("خطا:", error);
+      setErrorMessage(error?.message || "خطا در ارتباط با سرور");
+      setIsErrorModalOpen(true);
+    },
+  });
+
+  // SuccessModal
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    navigate("/login");
+  };
+
+  // ErrorModal
+  const handleCloseErrorModal = () => {
+    setIsErrorModalOpen(false);
+  };
+
+  // WarningModal
+  const handleCloseWarningModal = () => {
+    setIsWarningModalOpen(false);
+  };
+
+  return (
+    <>
+      <AuthForm
+        type="reset-password"
+        onSubmit={mutate}
+        isPending={isPending}
+        error={error?.message}
+        token={token}
+      />
+
+      <SuccessModal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        message={modalMessage}
+      />
+
+      <ErrorModal
+        isOpen={isErrorModalOpen}
+        onClose={handleCloseErrorModal}
+        message={errorMessage}
+      />
+
+      <WarningModal
+        isOpen={isWarningModalOpen}
+        onClose={handleCloseWarningModal}
+        message={warningMessage}
+        title=" توجه"
+      />
+    </>
+  );
 };
 
 export default ResetPasswordPage;
